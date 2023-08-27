284b073289e9fd585ba83d3af7e39133289a0085
Tiago Miotto <tiago.miotto@oulook.pt>
2023-08-27 12:54:00 +0100

    create project structure and initialization

---
+++ controllers/openai.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { OpenAI } = require("openai");
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+
+const recipeCompletion = async (request) => {
+    const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            "role": "system",
+            "content": "you are a helpful assistant specialized in building recipes that are healthy and protein rich. You will receive a list of ingredients and a time limit and generate easy step by step recipes that can be created using those parameters and also give the nutrional facts for the meal."
+        },
+        {
+            "role": "user",
+            "content": request
+        }],
+        temperature: 1.5,
+        max_tokens: 1130,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    return completion.choices[0].message.content;
+};
+
+module.exports = {
+    recipeCompletion
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var { recipeCompletion } = require('../controllers/openai');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  const response = await recipeCompletion("Give me a step by step recipe that I can cook with tomatoes, chicken breast, dry garlic, salt, pepper, milk and mayonaise in portuguese and with portions in grams");
+  console.log(JSON.stringify(response, undefined, 2));
+  res.render('index', { title: response });
 });
 
 module.exports = router;
