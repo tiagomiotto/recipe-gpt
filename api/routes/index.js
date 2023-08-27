37633268e8ffcca59987c0a3408aad57f11dc409
Tiago Miotto <tiago.miotto@oulook.pt>
2023-08-27 12:54:07 +0100

    Create project structure and initialization

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,7 +6,8 @@ var { recipeCompletion } = require('../controllers/openai');
 router.get('/', async function (req, res, next) {
   const response = await recipeCompletion("Give me a step by step recipe that I can cook with tomatoes, chicken breast, dry garlic, salt, pepper, milk and mayonaise in portuguese and with portions in grams");
   console.log(JSON.stringify(response, undefined, 2));
-  res.render('index', { title: response });
+  // res.render('index', { title: response });
+  res.sendFile('../public/index.html');
 });
 
 module.exports = router;
